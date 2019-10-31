d276bf07a5ef917563a8ceef94b7ac876a8528bd
August Money <august@planetexpress.io>
2019-10-30 22:42:59 -0500

    A different circle avatar. Look at the changes.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -39,8 +39,13 @@ class TransactionList extends StatelessWidget {
                 horizontal: 5
             ),
             child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
+              leading: Container(
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  shape: BoxShape.circle,
+                ),
                 child: Padding(
                   padding: EdgeInsets.all(6),
                   child: FittedBox(
